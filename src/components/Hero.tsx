aef33bc544b64214aa54c9ef76b1cc4a0b48db17
Ahmed <sc20amso@leeds.ac.uk>
2026-03-02 19:00:57 +0300

    fix: remove scroll indicator overlapping CTA buttons

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -105,21 +105,6 @@ export default function Hero() {
                     </a>
                 </motion.div>
 
-                {/* Scroll indicator */}
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ delay: 1.2 }}
-                    className="absolute bottom-8 left-1/2 -translate-x-1/2"
-                >
-                    <motion.div
-                        animate={{ y: [0, 8, 0] }}
-                        transition={{ duration: 1.5, repeat: Infinity }}
-                        className="w-6 h-10 rounded-full border-2 border-spora/40 flex items-start justify-center p-1.5"
-                    >
-                        <motion.div className="w-1.5 h-1.5 rounded-full bg-spora" />
-                    </motion.div>
-                </motion.div>
             </div>
         </section>
     );
